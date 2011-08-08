108d014277e02faee2d592fc3248060a3a41188c
simendsjo <simendsjo@gmail.com>
2011-08-08 16:55:36 +0200

    Add kernel32.lib to binary distribution

--- create_archive.bat
+++ create_archive.bat
@@ -1,1 +1,1 @@
-"c:\Program Files\7-Zip\7z.exe" a -tzip -mx9 -r dwin-%1.zip import lib\dwin.lib README LICENSE html
+"c:\Program Files\7-Zip\7z.exe" a -tzip -mx9 -r dwin-%1.zip import lib\dwin.lib lib\kernel32.lib README LICENSE html
